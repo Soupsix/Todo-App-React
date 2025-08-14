f07fa2fa187fcb781844061e937dda0cfb79149e
Soupsix <truongnvhe180438@fpt.edu.vn>
2025-08-14 13:23:07 +0700

    Example Components

--- react-basic/src/views/App.js
+++ react-basic/src/views/App.js
@@ -1,5 +1,15 @@
 import logo from './logo.svg';
 import './App.scss';
+import MyComponents from './Example/MyComponents';
+
+/**
+ * 2 loại component trong React:
+ * 1. Class component: sử dụng class để định nghĩa, có thể sử dụng state
+ * 2. Function component: sử dụng function để định nghĩa, không có state (trước đây), nhưng giờ đã có thể sử dụng state với React Hooks.
+ * 
+ * JSX là một hàm hay cú pháp của JS cho phép người dùng viết HTML trong JavaScript.
+ */
+
 
 function App() {
   return (
@@ -9,6 +19,7 @@ function App() {
         <p>
           Hello World with Soupsix!
         </p>
+        <MyComponents />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ react-basic/src/views/Example/MyComponents.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class MyComponents extends React.Component {
+  render() {
+    return (
+      <div className="my-components">
+        <h1>My Components</h1>
+        <p>This is a custom component.</p>
+      </div>
+    );
+  }
+}
+
+export default MyComponents;
